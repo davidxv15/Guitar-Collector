aac1387b05ac199ac4969bc3bbf0877855c524f4
davidxv15 <davidxvaz@gmail.com>
2023-03-29 21:48:31 -0700

    Guitar 1, PORT 8080

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
+guitars = [
+    {'model': 'Telecaster', 'manufacturer': 'Fender', 'description': 'solid-body electric guitar', 'strings': 6},
+    {'model': 'C5-CE', 'manufactuter': 'Cordoba', 'description': 'nylon-string electric classical guitar', 'strings': 6},
+]
 # Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def guitars_index(request):
+    return render(request, 'guitars/index.html', {
+        'guitars': guitars
+    })
