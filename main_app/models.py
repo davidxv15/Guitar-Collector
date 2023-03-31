a9769d15c3923f96ef1f814de66c333b74d1fc38
davidxv15 <davidxvaz@gmail.com>
2023-03-30 17:35:38 -0700

    Collector CRUD

--- main_app/models.py
+++ main_app/models.py
@@ -1,11 +1,15 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 class Guitar(models.Model):
     model = models.CharField(max_length=100)
     manufacturer = models.CharField(max_length=100)
-    description = models.CharField(max_length=250)
+    description = models.TextField(max_length=250)
     strings = models.IntegerField()
 
     def __str__(self):
         return self.model
+    
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'guitar_id': self.id})
--- main_app/views.py
+++ main_app/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 # importing guitar MODEL
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Guitar
 
 guitars = [
-    {'model': 'Telecaster', 'manufacturer': 'Fender', 'description': 'solid-body electric guitar', 'strings': 6},
-    {'model': 'C5-CE', 'manufacturer': 'Cordoba', 'description': 'nylon-string electric classical guitar', 'strings': 6},
+    {'model': 'Telecaster', 'manufacturer': 'Fender', 'description':'Solid-body electric guitar', 'strings': 6},
+    {'model': 'C5-CE', 'manufacturer': 'Cordoba', 'description':'Nylon-string electric classical guitar', 'strings': 6},
     {'model': 'Banjo', 'manufacturer': 'Fender', 'description': '5-string banjo', 'strings': 5},
 ]
 # Create your views here.
@@ -19,3 +20,22 @@ def guitars_index(request):
     return render(request, 'guitars/index.html', {
         'guitars': guitars
     })
+
+def guitars_detail(request, guitar_id):
+  guitar = Guitar.objects.get(id=guitar_id)
+  return render(request, 'guitars/detail.html', { 'guitar': guitar })
+
+class GuitarCreate(CreateView):
+    model = Guitar
+    fields = ['model', 'manufacturer', 'description', 'strings']
+    success_url = '/guitars/'
+    # {guitar_id}
+
+class GuitarUpdate(UpdateView):
+  model = Guitar
+  # Let's disallow the renaming of a cat by excluding the name field!
+  fields = ['model', 'manufacturer', 'description', 'strings']
+
+class GuitarDelete(DeleteView):
+  model = Guitar
+  success_url = '/guitars'
